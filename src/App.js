d790c5eb8b1230bc50ed21150f31bf2f4bc9e613
anshulkumarx7 <anshulkumarx7@gmail.com>
2023-04-07 23:56:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import "./Style/main.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="airbnb">
+        <Navbar />
+        <Hero />
+      </div>
     </div>
   );
 }
-
+//8.22
 export default App;
---
+++ src/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Style/Hero.css";
+function Hero() {
+  return (
+    <div className='Hero'>
+        <div className='airbnb-grid'>
+        </div>
+        <div className='hero-text'>
+          <h1>Online Experiences</h1>
+          <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
